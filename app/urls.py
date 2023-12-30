bfcab47ce8ab0894b95754cc59973253a4b38034
ratidzo <ratygore@gmail.com>
2023-12-30 11:56:25 +0200

    bug-fix: fix misspelled url in 'app/urls.py'

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
-    path('contrat/', views.congrat, name='congrat'),
+    path('congrat/', views.congrat, name='congrat'),
 ]
